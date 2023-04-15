86bd6bef11039d69ea66e3b902660a8bc88eabb1
ravali.mothukuri <ravali.mothukuri@eagles.oc.edu>
2023-04-14 22:26:16 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   title = 'angular-firebase-hosting';
+  albums: Array<any>;
+
+  constructor() {
+    this.albums = [
+      {
+        title: "One thing at a time",
+        singer: "Morgan Wallen"
+      },
+      {
+        title: "Sos",
+        singer: "SZA"
+      },
+      {
+        title: "Mañana Será Bonito",
+        singer: "Karol G"
+      }
+     ];
+  }
+
+  ngOnInit(): void {
+     
+  }
 }
